7606fbe88e9fdeae2ab036ea22e7222a183c66cb
Iyad Shobaki <iyadshobaki82@gmail.com>
2018-11-01 09:40:07 -0400

    first and last commit

---
+++ BLOBLab/BLOBLab/Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CreateDocumentStorageTableToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveBLOBToDatabseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FetchToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FetchBLOBFromDatabaseToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BLOBList = New System.Windows.Forms.ComboBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.CreateToolStripMenuItem, Me.SaveToolStripMenuItem, Me.FetchToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1643, 28)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExitToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
+        Me.FileToolStripMenuItem.Text = "File"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(108, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
+        '
+        'CreateToolStripMenuItem
+        '
+        Me.CreateToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CreateDocumentStorageTableToolStripMenuItem})
+        Me.CreateToolStripMenuItem.Name = "CreateToolStripMenuItem"
+        Me.CreateToolStripMenuItem.Size = New System.Drawing.Size(64, 24)
+        Me.CreateToolStripMenuItem.Text = "Create"
+        '
+        'CreateDocumentStorageTableToolStripMenuItem
+        '
+        Me.CreateDocumentStorageTableToolStripMenuItem.Name = "CreateDocumentStorageTableToolStripMenuItem"
+        Me.CreateDocumentStorageTableToolStripMenuItem.Size = New System.Drawing.Size(295, 26)
+        Me.CreateDocumentStorageTableToolStripMenuItem.Text = "Create Document Storage Table"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveBLOBToDatabseToolStripMenuItem})
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(52, 24)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'SaveBLOBToDatabseToolStripMenuItem
+        '
+        Me.SaveBLOBToDatabseToolStripMenuItem.Name = "SaveBLOBToDatabseToolStripMenuItem"
+        Me.SaveBLOBToDatabseToolStripMenuItem.Size = New System.Drawing.Size(239, 26)
+        Me.SaveBLOBToDatabseToolStripMenuItem.Text = "Save BLOB to Database"
+        '
+        'FetchToolStripMenuItem
+        '
+        Me.FetchToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FetchBLOBFromDatabaseToolStripMenuItem})
+        Me.FetchToolStripMenuItem.Name = "FetchToolStripMenuItem"
+        Me.FetchToolStripMenuItem.Size = New System.Drawing.Size(56, 24)
+        Me.FetchToolStripMenuItem.Text = "Fetch"
+        '
+        'FetchBLOBFromDatabaseToolStripMenuItem
+        '
+        Me.FetchBLOBFromDatabaseToolStripMenuItem.Name = "FetchBLOBFromDatabaseToolStripMenuItem"
+        Me.FetchBLOBFromDatabaseToolStripMenuItem.Size = New System.Drawing.Size(261, 26)
+        Me.FetchBLOBFromDatabaseToolStripMenuItem.Text = "Fetch BLOB from Database"
+        '
+        'BLOBList
+        '
+        Me.BLOBList.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BLOBList.FormattingEnabled = True
+        Me.BLOBList.Location = New System.Drawing.Point(31, 44)
+        Me.BLOBList.Name = "BLOBList"
+        Me.BLOBList.Size = New System.Drawing.Size(331, 33)
+        Me.BLOBList.TabIndex = 1
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(31, 105)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(1549, 632)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 2
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1643, 793)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.BLOBList)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Shobaki-BLOBLab"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents CreateDocumentStorageTableToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SaveBLOBToDatabseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FetchToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents FetchBLOBFromDatabaseToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents BLOBList As ComboBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ BLOBLab/BLOBLab/Form1.vb
@@ -0,0 +1,179 @@
+﻿Imports System.Data.SqlClient
+Imports System.IO
+
+Public Class Form1
+    Private NorthWindConnection As New SqlConnection(My.Settings.DB1ConnectionString)
+    Private CompleteFilePath As String = ""
+    Private SavePath As String = ""
+
+    Private Sub CreateDocumentStorageTableToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CreateDocumentStorageTableToolStripMenuItem.Click
+        Dim response As DialogResult =
+            MessageBox.Show("Create the Document Storage Table?" &
+                            "Click Yes to create a new DocumentStorage Table. " &
+                            "Click No to cancel",
+                            "Create DocumentStorage Table", MessageBoxButtons.YesNo,
+                            MessageBoxIcon.Question)
+        Select Case response
+            Case Is = Windows.Forms.DialogResult.Yes
+                CreateDocumentStorageTable()
+                MessageBox.Show("Table created successfully!")
+            Case Is = Windows.Forms.DialogResult.No
+                RefreshBlobList()
+        End Select
+
+    End Sub
+    Private Sub CreateDocumentStorageTable()
+        Dim CreateTableCommand As New SqlCommand
+        CreateTableCommand.Connection = NorthWindConnection
+        CreateTableCommand.CommandType = CommandType.Text
+        CreateTableCommand.CommandText =
+            "IF OBJECT_ID ('DocumentStorage' ) is not null " &
+            "Drop table DocumentStorage; " &
+            "create table DocumentStorage(" &
+            "DocumentID int identity(1,1) not null, " &
+            "FileName nvarchar(255) not null, " &
+            "DocumentFile varbinary(max) not null)"
+
+        CreateTableCommand.Connection.Open()
+        CreateTableCommand.ExecuteNonQuery()
+        CreateTableCommand.Connection.Close()
+
+    End Sub
+    Private Sub RefreshBlobList()
+        Dim GetBLOBListCommand As New SqlCommand(
+            "Select FileName from DocumentStorage", NorthWindConnection)
+        Dim reader As SqlDataReader
+        GetBLOBListCommand.Connection.Open()
+        reader = GetBLOBListCommand.ExecuteReader
+        BLOBList.Items.Clear()
+        While reader.Read
+            BLOBList.Items.Add(reader(0))
+        End While
+
+
+        reader.Close()
+        GetBLOBListCommand.Connection.Close()
+        BLOBList.SelectedIndex = 0
+
+    End Sub
+    Private Sub GetCompleteFilePath()
+        Dim OpenDialog As New OpenFileDialog
+        OpenDialog.Title = "Select Document File to Save"
+        OpenDialog.ShowDialog()
+        CompleteFilePath = OpenDialog.FileName
+    End Sub
+    Private Sub GetSavePath()
+        Dim SavePathDialog As New FolderBrowserDialog
+        SavePathDialog.Description = "Select a folder to restore BLOB file to"
+        SavePathDialog.ShowDialog()
+        SavePath = SavePathDialog.SelectedPath
+    End Sub
+    Private Sub SaveBLOBToDatabase()
+        GetCompleteFilePath()
+        Dim BLOB() As Byte
+        Dim FileStream As New IO.FileStream(CompleteFilePath, IO.FileMode.Open, IO.FileAccess.Read)
+        Dim reader As New IO.BinaryReader(FileStream)
+
+        BLOB = reader.ReadBytes(CInt(My.Computer.FileSystem.GetFileInfo(CompleteFilePath).Length))
+        FileStream.Close()
+        reader.Close()
+
+        Dim SaveDocCommand As New SqlCommand
+        SaveDocCommand.Connection = NorthWindConnection
+        SaveDocCommand.CommandText = "insert into DocumentStorage" &
+            "(FileName, DocumentFile)" &
+            "values (@FileName, @DocumentFile)"
+        Dim FileNameParameter As New SqlParameter("@FileName", SqlDbType.NChar)
+        Dim DocumentFileParameter As New SqlParameter("@DocumentFile", SqlDbType.Binary)
+        SaveDocCommand.Parameters.Add(FileNameParameter)
+        SaveDocCommand.Parameters.Add(DocumentFileParameter)
+
+        FileNameParameter.Value =
+            CompleteFilePath.Substring(CompleteFilePath.LastIndexOf("\") + 1)
+        DocumentFileParameter.Value = BLOB
+        Try
+            SaveDocCommand.Connection.Open()
+            SaveDocCommand.ExecuteNonQuery()
+            MessageBox.Show(FileNameParameter.Value.ToString &
+                            " Saved to database.", "BLOB Saved!", MessageBoxButtons.OK, MessageBoxIcon.Information)
+
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Saved Failed", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        Finally
+
+            SaveDocCommand.Connection.Close()
+
+        End Try
+
+
+    End Sub
+    Private Sub FetchBlobFromDatabsae()
+        If BLOBList.Text = "" Then
+            MessageBox.Show("select a BLOB to fetch from the ComboBox")
+            Exit Sub
+        End If
+        GetSavePath()
+        Dim GetBlobCommand As New SqlCommand(
+            "select FileName, DocumentFile " &
+            "from DocumentStorage " &
+            "where FileName = @DocName", NorthWindConnection)
+        GetBlobCommand.Parameters.Add("@DocName", SqlDbType.NVarChar).Value = BLOBList.Text
+
+        Dim CurrentIndex As Long = 0
+        Dim BufferSize As Integer = 100
+        Dim BytesReturned As Long
+        Dim Blob(BufferSize - 1) As Byte
+        GetBlobCommand.Connection.Open()
+        Dim reader As SqlDataReader =
+            GetBlobCommand.ExecuteReader(CommandBehavior.SequentialAccess)
+        Do While reader.Read
+            Dim FileStream As New IO.FileStream(SavePath & "\" & reader("FileName").ToString, IO.FileMode.OpenOrCreate, IO.FileAccess.Write)
+            Dim writer As New IO.BinaryWriter(FileStream)
+            CurrentIndex = 0
+            BytesReturned = reader.GetBytes(1, CurrentIndex, Blob, 0, BufferSize)
+
+            Do While BytesReturned = BufferSize
+                writer.Write(Blob)
+                writer.Flush()
+                CurrentIndex += BufferSize
+                BytesReturned = reader.GetBytes(1, CurrentIndex, Blob, 0, BufferSize)
+            Loop
+            writer.Write(Blob, 0, CInt(BytesReturned - 1))
+            writer.Flush()
+            writer.Close()
+            FileStream.Close()
+
+        Loop
+        reader.Close()
+        GetBlobCommand.Connection.Close()
+
+    End Sub
+
+    Private Sub SaveBLOBToDatabseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveBLOBToDatabseToolStripMenuItem.Click
+        SaveBLOBToDatabase()
+
+        RefreshBlobList()
+    End Sub
+
+    Private Sub FetchBLOBFromDatabaseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles FetchBLOBFromDatabaseToolStripMenuItem.Click
+        FetchBlobFromDatabsae()
+
+    End Sub
+
+    Private Sub BLOBList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles BLOBList.SelectedIndexChanged
+        Dim command As New SqlCommand("select FileName, DocumentFile from DocumentStorage where FileName = @FileName", NorthWindConnection)
+        command.Parameters.Add("@FileName", SqlDbType.NVarChar).Value = BLOBList.Text
+        Dim adapter As New SqlDataAdapter(command)
+        Dim table As New DataTable()
+        adapter.Fill(table)
+        Dim img() As Byte
+        img = table(0)(1)
+        Dim ms As New MemoryStream(img)
+        PictureBox1.Image = Image.FromStream(ms)
+
+    End Sub
+
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+End Class
